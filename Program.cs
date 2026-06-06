be44b50cee4c37ee26fa6499bbdda4b499610858
Salvatore Allegra <salallegra@gmail.com>
2026-06-06 08:17:18 -0700

    Setup Database Connection

--- Program.cs
+++ Program.cs
@@ -1,8 +1,12 @@
+using Infrastructure;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddToDoDbContext(builder.Configuration);  
 
 var app = builder.Build();
 
